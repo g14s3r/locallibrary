8d91cacaca33d7af4765feef65eaa1f0fffc07ce
g14s3r <g14s3r@gmail.com>
2020-01-31 22:29:06 +0300

    End 10 point

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$_c%@&1ujkpobo#xix2o(61=p3ffg__et@4_)etza5@p_@se1p'
+#SECRET_KEY = '$_c%@&1ujkpobo#xix2o(61=p3ffg__et@4_)etza5@p_@se1p'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '$_c%@&1ujkpobo#xix2o(61=p3ffg__et@4_)etza5@p_@se1p')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
